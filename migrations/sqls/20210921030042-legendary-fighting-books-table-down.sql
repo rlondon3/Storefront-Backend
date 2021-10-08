1931faa7f0c6b7fa5600225a45bb6a1806b2aa86
Raphael Smith <codesmithr@outlook.com>
2021-10-08 00:06:30 -0700

    Fix ENV and SQL File fix

--- migrations/sqls/20210921030042-legendary-fighting-books-table-down.sql
+++
@@ -1,1 +0,0 @@
-DROP TABLE legendary_fighting_books;
--- migrations/sqls/20210921030042-legendary-fighting-books-table-up.sql
+++
@@ -1,3 +0,0 @@
-CREATE TABLE legendary_fighting_books (title VARCHAR(100), author VARCHAR(100), synopsis text, published_era VARCHAR(100), pages integer, last_known_citing VARCHAR(200), id SERIAL PRIMARY KEY );
-
-
