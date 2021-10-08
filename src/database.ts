97d8052282f31001d6265554508388d294fb6d03
Raphael Smith <codesmithr@outlook.com>
2021-10-08 00:12:00 -0700

    Database.ts file fix

--- src/database.ts
+++ src/database.ts
@@ -3,8 +3,14 @@ import { Pool } from 'pg';
 
 dotenv.config();
 
-const { POSTGRES_HOST, POSTGRES_DB, POSTGRES_USER, POSTGRES_PASSWORD, ENV } =
-  process.env;
+const {
+  POSTGRES_HOST,
+  POSTGRES_DB,
+  POSTGRES_TEST_DB,
+  POSTGRES_USER,
+  POSTGRES_PASSWORD,
+  ENV,
+} = process.env;
 
 let client = new Pool();
 
@@ -20,7 +26,7 @@ if (ENV === 'dev') {
 if (ENV === 'test') {
   client = new Pool({
     host: POSTGRES_HOST,
-    database: POSTGRES_DB,
+    database: POSTGRES_TEST_DB,
     user: POSTGRES_USER,
     password: POSTGRES_PASSWORD,
   });
